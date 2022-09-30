2eaa996d13de631ac3731bb845228a21109eb8a6
durotemy <durotemy@gmail.com>
2022-09-30 22:22:23 +0100

    exercise card implemented

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+
     console.log("exercises", exercises)
     return (
         <Box id="exercises"
@@ -11,6 +13,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
             mt="50px"
         >
             <Typography variant="h3" mb="46px">
+
                 Showing Results
             </Typography>
             <Stack spacing={2}
@@ -19,17 +22,18 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
                 justifyContent="center"
                 sx={{ gap: { lg: '110px', xs: '50px' } }}>
                 {exercises.map((exercise) => (
-                    <Box key={exercise.id} sx={{ width: { lg: "400px", xs: "300px" } }}>
-                        <Box sx={{ height: { lg: "400px", xs: "300px" }, width: { lg: "400px", xs: "300px" }, borderRadius: "20px", overflow: "hidden" }}>
-                            <img src={exercise.gifUrl} alt={exercise.name} />
-                        </Box>
-                        <Typography variant="h5" mt="20px" mb="10px">
-                            {exercise.name}
-                        </Typography>
-                        {/* <Typography variant="body1" mb="20px">
-                            {exercise.description}
-                        </Typography> */}
-                    </Box>
+                    // <Box key={exercise.id} sx={{ width: { lg: "400px", xs: "300px" } }}>
+                    //     <Box sx={{ height: { lg: "400px", xs: "300px" }, width: { lg: "400px", xs: "300px" }, borderRadius: "20px", overflow: "hidden" }}>
+                    //         <img src={exercise.gifUrl} alt={exercise.name} />
+                    //     </Box>
+                    //     <Typography variant="h5" mt="20px" mb="10px">
+                    //         {exercise.name.toUpperCase()}
+                    //     </Typography>
+                    //     <Typography variant="body1" mb="20px">
+                    //         {exercise.bodyPart.toUpperCase()}
+                    //     </Typography>
+                    // </Box>
+                    <ExerciseCard exercise={exercise} />
                 ))}
             </Stack>
 
