989e05315e1ba8f764da215151b5d51e67eeccb4
durotemy <durotemy@gmail.com>
2022-09-30 13:05:24 +0100

    exercises added

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,7 +3,8 @@ import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 
-const ExerciseDetail = ({ exercises, setExercises, bodyPart }) => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+    console.log("exercises", exercises)
     return (
         <Box id="exercises"
             sx={{ mt: { lg: "100px" } }}
@@ -12,8 +13,29 @@ const ExerciseDetail = ({ exercises, setExercises, bodyPart }) => {
             <Typography variant="h3" mb="46px">
                 Showing Results
             </Typography>
+            <Stack spacing={2}
+                direction="row"
+                flexWrap="wrap"
+                justifyContent="center"
+                sx={{ gap: { lg: '110px', xs: '50px' } }}>
+                {exercises.map((exercise) => (
+                    <Box key={exercise.id} sx={{ width: { lg: "400px", xs: "300px" } }}>
+                        <Box sx={{ height: { lg: "400px", xs: "300px" }, width: { lg: "400px", xs: "300px" }, borderRadius: "20px", overflow: "hidden" }}>
+                            <img src={exercise.gifUrl} alt={exercise.name} />
+                        </Box>
+                        <Typography variant="h5" mt="20px" mb="10px">
+                            {exercise.name}
+                        </Typography>
+                        {/* <Typography variant="body1" mb="20px">
+                            {exercise.description}
+                        </Typography> */}
+                    </Box>
+                ))}
+            </Stack>
+
+
         </Box>
     )
 }
 
-export default ExerciseDetail;
+export default Exercises;
