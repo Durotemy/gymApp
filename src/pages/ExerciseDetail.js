e565b05a5d0b021c58f0ac0aae0f4e2130ec4ff3
durotemy <durotemy@gmail.com>
2022-10-01 20:30:28 +0100

    exerciseDetailPage

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import Pagination from "@mui/material/Pagination";
+import { useParams } from 'react-router-dom';
 import { Box, Stack, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+import Details from '../components/Detail';
 
 const ExerciseDetail = ({ exercises, setExercises, bodyPart }) => {
     return (
@@ -9,7 +12,10 @@ const ExerciseDetail = ({ exercises, setExercises, bodyPart }) => {
             sx={{ mt: { lg: "100px" } }}
             mt="50px"
         >
-            
+            <Details />
+            <ExerciseVideos />
+            <SimilarExercises />
+
         </Box>
     )
 }
